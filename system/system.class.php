f42368e1642fa42a7de1b80a8bdc785b8293cbaf
JD <piotrgiedziun@gmail.com>
2011-09-06 17:49:11 +0200

    move get class, method, parms logic form index to system class System::load class parm change, now all files must end ".class.php"

--- system/system.class.php
+++ system/system.class.php
@@ -5,12 +5,96 @@
  */
 class System {
     
+    /*
+     * only static acces 
+     */
+    private function __construct() {}
+    
+    /*
+     * System initialization
+     * extracts from the address URL logic of class, methods, parms
+     */
+    public static function init() {
+        /*
+         * extracting information form GET request
+         * remove first slash, get first index element form $_GET
+         */
+        $get_key = substr(key($_GET), 1);
+
+        /*
+         * explode when there is more data
+         * eg.
+         * Following URL will be converted to array of elements
+         * controller/function/parm1 => array('contoller', 'function', 'parm1')
+         */
+        if(is_numeric(strpos($get_key, '/'))) 
+            $get_keys = explode('/', $get_key);
+        else 
+            $get_keys = array($get_key);
+
+        /*
+         * Only alphanumeric characters are allowed
+         * otherwise throws 404
+         */
+        foreach($get_keys as $value)
+            if($value!= '' && !preg_match('/^[a-zA-Z0-9]*$/', $value))
+                self::show_404('URL constains disallowed characters.');
+
+        /*
+         * class name extraction logic
+         */
+        if(isset($get_keys[0]) && file_exists('controllers/'.strtolower($get_keys[0]).'.class.php'))
+            $class = strtolower($get_keys[0]);
+        elseif(isset($get_keys[0]) && strlen($get_keys[0]) != 0) 
+            self::show_404('Invalid class');
+        else
+            $class = 'index';
+
+        self::load_controller(strtolower($class));
+
+        
+        /*
+         * Create instans of $class
+         */
+        $class_obj = new $class();
+
+        /**
+         * method name extraction logic
+         */
+        if(isset($get_keys[1]) && method_exists($class_obj, strtolower($get_keys[1])))
+            $method = strtolower($get_keys[1]);
+        elseif(isset($get_keys[1]) && strlen($get_keys[1]) != 0)
+            self::show_404('Invalid method');
+        else
+            $method = 'index';
+        
+        /*
+         * parms extraction logic
+         */
+        $parms = array();
+        for($i = 2; $i<count($get_keys); $i++)
+            array_push($parms, $get_keys[$i]);
+
+        /*
+         * call class method with parms
+         */
+        call_user_func_array(array($class_obj, $method), $parms);
+    }
+    
+     /**
+     * Load contoller class
+     * @param String $class_name 
+     */
+    public static function load_controller($class_name) {
+        self::load('controllers', $class_name);
+    }
+    
     /**
      * Load model class
      * @param String $class_name 
      */
     public static function load_model($class_name) {
-        self::load($class_name, 'models/'.$class_name.'.class.php');
+        self::load('models', $class_name);
     }
     
     /**
@@ -18,18 +102,18 @@ class System {
      * @param String $class_name 
      */
     public static function load_system($class_name) {
-        self::load($class_name, 'system/'.$class_name.'.class.php');
+        self::load('system', $class_name);
     }
     
     /*
-     * @parm String $file_name - display in error log
-     * @param String $file_path - path to file
+     * @parm String $dir 
+     * @param String $file_name
      */
-    private static function load($file_name, $file_path) {
-        if(!file_exists($file_path))
-            self::show_404('[SYSTEM ERROR] file '.$file_name.' not found.');
+    private static function load($dir, $file_name) {
+        if(!file_exists($dir.'/'.$file_name.'.class.php'))
+            self::show_404('[SYSTEM ERROR] file '.$file_name.' ('.$dir.') not found.');
         
-        require_once($file_path);
+        require_once($dir.'/'.$file_name.'.class.php');
     }
     
     /*
